eab4067de4208a06db03c0ccacaba1234c4102af
Bello Emmanuel <-m>
2023-04-29 23:23:34 +0100

    changes to js

--- login.js
+++ login.js
@@ -7,10 +7,18 @@ const passwordMessage = document.getElementById('password-message')
 createAccountBtn.addEventListener('click', getValue)
 
 function getValue(){
-if(emailMessage.value != String('belloemmanuel02@gmail.com')){
+if(email.value != String('belloemmanuel02@gmail.com' || email.value == '')){
 emailMessage.innerHTML = "please enter correct email"
 emailMessage.style.color = 'red'
 } else{
+    emailMessage.innerHTML = " "
     passwordMessage.innerHTML = 'please enter password'
 } 
+if(password.value != Number(12345)){
+    passwordMessage.innerHTML = 'please enter correct password'
+    passwordMessage.style.color = 'red'
+} else{
+    passwordMessage.innerHTML = " "
+    emailMessage.innerHTML = 'please enter email'
+}
 }
