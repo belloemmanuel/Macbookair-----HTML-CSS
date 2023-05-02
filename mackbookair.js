32ca75da090c6712836398acf9d569dbcf57e579
Bello Emmanuel <-m>
2023-05-02 13:45:53 +0100

    js changes

--- mackbookair.js
+++ mackbookair.js
@@ -5,6 +5,9 @@ const eye = document.getElementById('fa-eye');
 const faEyeSlash = document.getElementById('fa-eye-slash');
 const btn = document.getElementById('creatAccnt');
 const verifybtn = document.getElementById('verifybtn')
+const nameMessage = document.getElementById('nameMessage')
+const emailMessage = document.getElementById('emailMessage')
+const passwordMessage = document.getElementById('passwordMessage')
 
 
 btn.addEventListener('click', getValue);
@@ -24,8 +27,7 @@ function revealPassword() {
   faEyeSlash.style.display = 'block';
 }
 
-function getValue() {
-  console.log(`Username: ${username.value}`);
-  console.log(`Email: ${email.value}`);
-  console.log(`Password: ${password.value}`);
-}
+const myPassword = '12345'
+const myEmail = 'belloemmanuel02@gmail.com'
+const myUsername = 'Bello Emmanuel'
+
