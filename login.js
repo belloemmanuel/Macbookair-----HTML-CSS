e5929993d01810af5a99af64231bfbb429cac60f
belloemmanuel <-m>
2023-05-03 13:29:43 +0100

    changes made to js

--- login.js
+++ login.js
@@ -11,18 +11,34 @@ let myPassword = '12345'
 
 function getValue(){
    if(email.value == ''){
-    emailMessage.innerHTML = 'please enter email'
+    emailMessage.innerHTML = 'Please Enter Email'
    } else if(email.value !== myEmail){
-    emailMessage.innerHTML = 'please enter correct email'
+    emailMessage.innerHTML = 'Please Enter Correct Email'
    } else{
     emailMessage.innerHTML = ''
-    passwordMessage.innerHTML = 'please enter password'
+    passwordMessage.innerHTML = 'Please Enter Password'
    }
    if(password.value == ''){
-    passwordMessage.innerHTML = 'please enter password'
+    passwordMessage.innerHTML = 'Please Enter Password'
    } else if(password.value !== myPassword){
-    passwordMessage.innerHTML = 'please enter correct password'
+    passwordMessage.innerHTML = 'Please Enter Correct Password'
    } else{
     passwordMessage.innerHTML = ''
    }
+}
+
+const strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+
+password.addEventListener('keyup', checkRegularExp)
+
+function checkRegularExp(){
+if(!password.value.match(strongRegex)){
+console.log('RegExp Error')
+passwordMessage.innerHTML = 'Looks Too Simple'
+passwordMessage.style.color = 'red'
+}else{
+   console.log('passed')
+   passwordMessage.innerHTML = 'This is Perfect'
+passwordMessage.style.color = 'green'
+}
 }
